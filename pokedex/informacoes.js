51e705d9157b22b1ce4e015e20ba7433e4f12211
Roger Costa <roger.desenv@gmail.com>
2023-04-09 16:45:28 -0300

    add primeira estrutura de html

---
+++ pokedex/informacoes.js
@@ -0,0 +1,19 @@
+/*
+
+Obs.: Instação do http-server nesta pasta para simular um servidor real;
+
+- Instalação: npm install -g http-server;
+
+- Verificar a versão: http-server -v;
+
+- Para rodar o servidor: http-server;
+
+- Available on:
+   http://192.168.0.48:8080
+   http://127.0.0.1:8080
+*/
+
+/*
+Pasta assets: Javascript, css, imagens. HTML não entra.
+*/
+
